94371965289ad8bbb40b6282c5c1e258b25d918a
matsusyo <44990628+matsusyo@users.noreply.github.com>
2019-06-13 16:49:36 +0900

    Add files via upload

---
+++ Data/chip.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="chip5" tilewidth="64" tileheight="64" tilecount="12" columns="4">
+ <image source="../Image/chip5.png" width="256" height="192"/>
+</tileset>
